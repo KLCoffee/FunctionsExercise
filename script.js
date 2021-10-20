f29c52d5e5576111fd9a1cafc821e276bf4b2065
Kathy Coffee <klcoffee8807@gmail.com>
2021-10-20 08:54:42 -0400

    Java Script Functions Exercise

---
+++ script.js
@@ -0,0 +1,91 @@
+// FUNCTIONS EXERCISE
+
+// 1
+function spookyGhost(){
+    console.log(`Boo!!!`);
+}
+spookyGhost();
+
+// 2
+function candy(goal){
+    console.log(`${goal}! ${goal}! ${goal}! ${goal}!`);
+}
+candy(`Get Candy`);
+
+// 3
+function werewolf (a,b){
+    if (a===`Full` && b===`Moon`){
+        return `ARH-WOO!`;
+    }
+}
+console.log(`Full` `Moon`);
+
+// 4
+function halloween (){
+ return`October 31`;   
+}
+const trickTreat = halloween();
+console.log(trickTreat);
+
+// // 5
+// function pieces(){
+//     if (pieces >= 31){
+//         return `True`;
+//     }else (pieces <= 31){
+//         return `False`;
+//     }
+// }
+// console.log();
+
+// 6
+candyCounter = (numOne, numTwo, numThree) => numOne + numTwo + numThree;
+const num = candyCounter(11, 20, 3);
+console.log(num);
+// 7
+const hauntedMansion = function (){
+console.log(`Welcome, foolish mortals, to the Haunted Mansion!`);
+}
+hauntedMansion();
+
+// 8
+ghostBusters = () => console.log(`Who You Gonna Call?`);
+
+ghostBusters();
+
+// 9 No
+//10  Yes
+
+// // 11a
+// const costumes =[`Ghost`, 
+//                 `Princess`,
+//                 `Pirate`
+    
+// ];
+// // 11b
+// function costumes (){
+//     console.log(upperCostumes);
+// }
+// upperCostumes.map(toUppercase);
+
+
+// 12 a
+const stephenKingMovies=[`The Shining`, 
+                        `Christine`, 
+                        `It`, 
+                        `The Mist`, 
+                        `Creepshow`,
+                        `Pet Sematary`
+                        ];
+
+
+ // 13a
+const halloweenSongs1 = [
+    `Monster Mash`,
+    `Thriller`
+]
+ // 13b
+const halloweenSongs2 = [
+    `I Put A Spell On You`,
+    `This Is Halloween`,
+    `Ghostbusters`
+]
